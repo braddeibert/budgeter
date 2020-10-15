53fc2ec7bdee924f66f5d338ef854afbd5d18209
Brad Deibert <bradeibert7@gmail.com>
2020-10-15 12:38:36 -0600

    create models for budgeter app

--- Budgeter/models.py
+++ Budgeter/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Budget(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return (reverse('budget', args=[str(self.id)]))        
+
+class Expense(models.Model):
+    budget = models.ForeignKey(Budget)
+    name = models.CharField(max_length=64)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.name + ' ' + self.amount
+
+    def get_absolute_url(self):
+        return (reverse('expense', args=[str(self.id)]))
+
+class Purchases(models.Model):
+    expense = models.ForeignKey(Expense)
+    name = models.CharField(max_length=64)
+    date = models.DateField()
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.date + ' ' + self.name + ' ' + self.amount
+
+    def get_absolute_url(self):
+        return (reverse('purchase', args=[str(self.id)]))
--- cs291_project/settings.py
+++ cs291_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Budgeter', 
+    'Users'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'cs291_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
