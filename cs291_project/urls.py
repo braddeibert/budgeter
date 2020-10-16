0dd72c31754def53a4a1e794c48f27ea70a88eb4
Brad Deibert <bradeibert7@gmail.com>
2020-10-15 20:41:48 -0600

    next up: adding views

--- cs291_project/urls.py
+++ cs291_project/urls.py
@@ -18,4 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('django.contrib.auth.urls')),
+    path('users/', include('Users.urls')),
+    path('', include('Budgeter.urls')),
 ]
