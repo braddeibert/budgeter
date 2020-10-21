b598c801dad02f5c4b00d0c484f9828bd282199a
Brad Deibert <bradeibert7@gmail.com>
2020-10-20 22:07:01 -0600

    added css (including framework Skeleton) and sass (css preprocessor) to project

--- Budgeter/urls.py
+++ Budgeter/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import BudgetListView
+
 
 urlpatterns = [
-    
+    path('', BudgetListView.as_view(), name='budget-list'),
 ]
--- Budgeter/views.py
+++ Budgeter/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import View
+from django.views.generic import TemplateView, ListView
+
+from .models import Budget, Expense, Purchase
 
 # Create your views here.
+class BudgetListView(ListView):
+    model = Budget
+    
+    template_name = 'budget.html'
+
+class ExpenseView(TemplateView):
+    template_name = 'expense.html'
+
+class PurchaseView(TemplateView):
+    template_name = 'purchase.html'
