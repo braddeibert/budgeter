eb35d127d6aba67b5d58219b20f366a694e42d1e
Brad Deibert <bradeibert7@gmail.com>
2020-11-14 19:41:35 -0700

    Change settings

--- cs291_project/settings.py
+++ cs291_project/settings.py
@@ -130,7 +130,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
     
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_REDIRECT_URL = '/'
 
 # Activating django-heroku
