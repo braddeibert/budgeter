f1312242ef23ec761127a5d1a58e702d86f101c1
Brad Deibert <bradeibert7@gmail.com>
2020-11-14 18:55:43 -0700

    More configs

--- cs291_project/settings.py
+++ cs291_project/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 if DEBUG:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 else: 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
